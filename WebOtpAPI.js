d03df28b534b4f49bd686dc838f7f54e3695aa9e
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-02 13:36:32 +0530

    rollback

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,35 +1,34 @@
-export function autoReadSMS(cb) {
-  // Used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1 minute
-  const signal = new AbortController();
-  setTimeout(() => {
-    signal.abort();
-  }, 1 * 60 * 1000);
-
-  async function main() {
-    if ('OTPCredential' in window) {
-      try {
-        if (navigator.credentials) {
+  function autoReadSMS(cb) {
+    alert("inside funvction");
+    // used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1min
+     const signal = new AbortController();
+     setTimeout(() => {
+       signal.abort();
+     }, 1 * 60 * 1000);
+     async function main() {
+       if ('OTPCredential' in window) {
           try {
-            await navigator.credentials
-              .get({ abort: signal, otp: { transport: ['sms'] } })
-              .then(content => {
-                if (content && content.code) {
-                  const otpRegex = /OTP:(\d{4})/; // Regex to match OTP format
-                  const match = content.code.match(otpRegex);
-                  if (match && match[1]) {
-                    cb(match[1]); // Pass the extracted OTP to the callback function
-                  }
+             if (navigator.credentials) {
+                try {
+                   await navigator.credentials
+                   .get({ abort: signal, otp:{ transport: ['sms']}})
+                   .then(content => {
+                     if (content && content.code) {
+                       cb(content.code); 
+                     }
+                   })
+                   .catch(e => console.log(e));
+                } 
+                catch (e) {
+                  return;
                 }
-              })
-              .catch(e => console.log(e));
-          } catch (e) {
-            return;
+             }
+          } 
+          catch (err) {
+            console.log(err);
           }
         }
-      } catch (err) {
-        console.log(err);
-      }
+     }
+     main();
     }
-  }
-  main();
-}
+    
