b10ac279b817cd36329ca0b3d5f0fe52078c1aa2
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-02 16:41:00 +0530

    Update WebOtpAPI.js

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,32 +1,22 @@
-// Detect feature support via OTPCredential availability
-if ("OTPCredential" in window) {
-    window.addEventListener("DOMContentLoaded", (e) => {
-      const input = document.querySelector('input[autocomplete="one-time-code"]');
-      if (!input) return;
-      // Set up an AbortController to use with the OTP request
-      const ac = new AbortController();
-      const form = input.closest("form");
-      if (form) {
-        // Abort the OTP request if the user attempts to submit the form manually
-        form.addEventListener("submit", (e) => {
-          ac.abort();
-        });
-      }
-      // Request the OTP via get()
-      navigator.credentials
-        .get({
-          otp: { transport: ["sms"] },
-          signal: ac.signal,
-        })
-        .then((otp) => {
-          // When the OTP is received by the app client, enter it into the form
-          // input and submit the form automatically
-          input.value = otp.code;
-          if (form) form.submit();
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+if ('OTPCredential' in window) {
+  window.addEventListener('DOMContentLoaded', e => {
+    const input = document.querySelector('input[autocomplete="one-time-code"]');
+    if (!input) return;
+    const ac = new AbortController();
+    const form = input.closest('form');
+    if (form) {
+      form.addEventListener('submit', e => {
+        ac.abort();
+      });
+    }
+    navigator.credentials.get({
+      otp: { transport:['sms'] },
+      signal: ac.signal
+    }).then(otp => {
+      input.value = otp.code;
+      if (form) form.submit();
+    }).catch(err => {
+      console.log(err);
     });
-  }
-  
+  });
+}
