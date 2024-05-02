d3b0d32bdf3f22917c734c0204c6e64566222922
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-02 14:33:29 +0530

    Update WebOtpAPI.js

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,32 +1,34 @@
  function autoReadSMS(cb) {
-// used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1min
- const signal = new AbortController();
- setTimeout(() => {
-   signal.abort();
- }, 1 * 60 * 1000);
- async function main() {
-   if ('OTPCredential' in window) {
-      try {
-         if (navigator.credentials) {
-            try {
-               await navigator.credentials
-               .get({ abort: signal, otp:{ transport: ['sms']}})
-               .then(content => {
-                 if (content && content.code) {
-                   cb(content.code);
-                 }
-               })
-               .catch(e => console.log(e));
-            } 
-            catch (e) {
-              return;
-            }
-         }
-      } 
-      catch (err) {
-        console.log(err);
-      }
+    alert("inside funvction");
+    // used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1min
+     const signal = new AbortController();
+     setTimeout(() => {
+       signal.abort();
+     }, 1 * 60 * 1000);
+     async function main() {
+       if ('OTPCredential' in window) {
+          try {
+             if (navigator.credentials) {
+                try {
+                   await navigator.credentials
+                   .get({ abort: signal, otp:{ transport: ['sms']}})
+                   .then(content => {
+                     if (content && content.code) {
+                       cb(content.code); 
+                     }
+                   })
+                   .catch(e => console.log(e));
+                } 
+                catch (e) {
+                  return;
+                }
+             }
+          } 
+          catch (err) {
+            console.log(err);
+          }
+        }
+     }
+     main();
     }
- }
- main();
-}
+    
