164c5c2803d4028eabaf221a0de0865d9da940ee
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-02 12:20:28 +0530

    Add files via upload

---
+++ WebOtpAPI.js
@@ -0,0 +1,26 @@
+if ('OTPCredential' in window) {
+    window.addEventListener('DOMContentLoaded', e => {
+      const input = document.querySelector('input[autocomplete="one-time-code"]');
+      if (!input) return;
+      // Cancel the Web OTP API if the form is submitted manually.
+      const ac = new AbortController();
+      const form = input.closest('form');
+      if (form) {
+        form.addEventListener('submit', e => {
+          // Cancel the Web OTP API.
+          ac.abort();
+        });
+      }
+      // Invoke the Web OTP API
+      navigator.credentials.get({
+        otp: { transport:['sms'] },
+        signal: ac.signal
+      }).then(otp => {
+        input.value = otp.code;
+        // Automatically submit the form when an OTP is obtained.
+        if (form) form.submit();
+      }).catch(err => {
+        console.log(err);
+      });
+    });
+  }
