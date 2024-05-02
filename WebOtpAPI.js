68dbb9f931f72a65501746b6a7f124b3fd1494c6
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-03 00:49:04 +0530

    up2

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,4 +1,3 @@
 navigator.credentials.get({
   otp: { transport: ['sms'] }
-})
-.then(otp => document.getElementById('otp-input').value = otp.code);
+}).then(otp => document.getElementById('otp-input').value = otp.code);
