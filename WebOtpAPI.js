315d47d7e062a85d1c9f947fcd4d0afec4faac24
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-02 13:32:01 +0530

    New implimentation

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,34 +1,35 @@
- function autoReadSMS(cb) {
-    alert("inside funvction");
-    // used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1min
-     const signal = new AbortController();
-     setTimeout(() => {
-       signal.abort();
-     }, 1 * 60 * 1000);
-     async function main() {
-       if ('OTPCredential' in window) {
+export function autoReadSMS(cb) {
+  // Used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1 minute
+  const signal = new AbortController();
+  setTimeout(() => {
+    signal.abort();
+  }, 1 * 60 * 1000);
+
+  async function main() {
+    if ('OTPCredential' in window) {
+      try {
+        if (navigator.credentials) {
           try {
-             if (navigator.credentials) {
-                try {
-                   await navigator.credentials
-                   .get({ abort: signal, otp:{ transport: ['sms']}})
-                   .then(content => {
-                     if (content && content.code) {
-                       cb(content.code); 
-                     }
-                   })
-                   .catch(e => console.log(e));
-                } 
-                catch (e) {
-                  return;
+            await navigator.credentials
+              .get({ abort: signal, otp: { transport: ['sms'] } })
+              .then(content => {
+                if (content && content.code) {
+                  const otpRegex = /OTP:(\d{4})/; // Regex to match OTP format
+                  const match = content.code.match(otpRegex);
+                  if (match && match[1]) {
+                    cb(match[1]); // Pass the extracted OTP to the callback function
+                  }
                 }
-             }
-          } 
-          catch (err) {
-            console.log(err);
+              })
+              .catch(e => console.log(e));
+          } catch (e) {
+            return;
           }
         }
-     }
-     main();
+      } catch (err) {
+        console.log(err);
+      }
     }
-    
+  }
+  main();
+}
