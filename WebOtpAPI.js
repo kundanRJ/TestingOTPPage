c8a4ad1edd8054ea9b962834716a9334857bc357
kundanRJ <144376639+kundanRJ@users.noreply.github.com>
2024-05-03 00:41:44 +0530

    Update WebOtpAPI.js

--- WebOtpAPI.js
+++ WebOtpAPI.js
@@ -1,23 +1,4 @@
-if ('OTPCredential' in window) {
-  window.addEventListener('DOMContentLoaded', e => {
-    const input = document.querySelector('input[autocomplete="one-time-code"]');
-    if (!input) return;
-    const ac = new AbortController();
-    const form = input.closest('form');
-    if (form) {
-      form.addEventListener('submit', e => {
-        ac.abort();
-      });
-    }
-    navigator.credentials.get({
-      otp: { transport:['sms'] },
-      signal: ac.signal
-    }).then(otp => {
-      input.value = otp.code;
-      alert("getting otp : " +input.value );
-      if (form) form.submit();
-    }).catch(err => {
-      console.log(err);
-    });
-  });
-}
+navigator.credentials.get({
+  otp: { transport: ['sms'] }
+})
+.then(otp => document.getElementById('otp-input').value = otp.code);
